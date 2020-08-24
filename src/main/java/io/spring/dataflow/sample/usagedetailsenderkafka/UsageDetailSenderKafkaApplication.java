1a4bb142d7f25bd4a2a4536a4043b2b943cff259
Manik Narula <manikmaxster@gmail.com>
2020-08-24 20:31:11 +0530

    Init

---
+++ src/main/java/io/spring/dataflow/sample/usagedetailsenderkafka/UsageDetailSenderKafkaApplication.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.sample.usagedetailsenderkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageDetailSenderKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageDetailSenderKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/dataflow/sample/usagedetailsenderkafka/UsageDetailSenderKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.sample.usagedetailsenderkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageDetailSenderKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
